6cd442062d5747b21f6b79c112e25a6016c17266
R-Github04 <152078172+R-Github04@users.noreply.github.com>
2024-07-27 09:20:15 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.py
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.py
@@ -6,20 +6,15 @@ for i in map(int, input().split()) :
 y, m = 0, 0
 for i in li :
     yc, mc = 1, 1
-    # case Y 
+    
     yc += int(i / 30)
-    #print("yc :", yc)
-    # case M
     mc += int(i / 60)
-    #print("mc :", mc)
+    
     y += yc
     m += mc
 
 y = y * 10
 m = m * 15
-
-#print("y :", y)
-#print("m :", m)
 
 if y == m : 
     print("Y M", y)
